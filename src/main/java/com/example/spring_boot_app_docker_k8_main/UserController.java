5ca973ab981f62d0106d34d286c464db644517aa
Shrilayan <iamshrilayan@gmail.com>
2024-05-28 16:16:49 +0530

    jenkins

---
+++ src/main/java/com/example/spring_boot_app_docker_k8_main/UserController.java
@@ -0,0 +1,24 @@
+package com.example.spring_boot_app_docker_k8_main;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping
+    public ResponseEntity<String> getUsers(){
+        String query="select count(*) from roles";
+        Integer count = jdbcTemplate.queryForObject(query, Integer.class);
+        return  new ResponseEntity<>("Total Users are " + count, HttpStatus.OK);
+    }
+}
